c7a5393a115c67cd6aaa5b4aa1be40bc054f3865
aronaNg <arona.ngom07@gmail.com>
2024-04-15 19:26:05 +0200

    edit header component

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -14,7 +14,7 @@ export class HeaderComponent implements OnInit {
   logged_in: boolean = false;
   user_role!: any;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
 
@@ -31,5 +31,10 @@ export class HeaderComponent implements OnInit {
     sessionStorage.removeItem("role");
     this.router.navigateByUrl('/sign-in');
     location.reload();
+  }
+  getImagePath() {
+    // Example: Assuming imagePath is dynamically set
+    let imagePath = '/assets/hand3.webp';
+    return this.sanitizer.bypassSecurityTrustResourceUrl(imagePath);
   }
 }
